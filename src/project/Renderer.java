974f3bc5f8bcfc27660b4a5f9860c7de71b5a7f7
Skadi999 <vj.novak9@gmail.com>
2022-03-22 16:46:09 +0100

    Corrected rotation, finished SimpleTree.

--- src/project/Renderer.java
+++ src/project/Renderer.java
@@ -14,6 +14,7 @@ import java.util.Stack;
 
 import static org.lwjgl.opengl.GL11.*;
 
+//todo refactor L system into its own class.
 //Angles: 0=right, 90=up, 180=left, 270=bottom
 public class Renderer extends AbstractRenderer {
 
@@ -30,13 +31,12 @@ public class Renderer extends AbstractRenderer {
     private RotationManager rotationManager;
     private int rotAngle;
 
-    private String axiom;
     private String lSystem;
     private int generationCount;
 
     private List<Rule> rules = new ArrayList<>();
 
-    private Stack<State> states = new Stack<>();
+    private final Stack<State> states = new Stack<>();
 
     public Renderer() {
         super(800, 800);
@@ -111,27 +111,26 @@ public class Renderer extends AbstractRenderer {
     }
 
     private void initializeLSystem() {
-//        Pattern pattern = new SimpleTree();
-        Pattern pattern = new KochIsle();
+        Pattern pattern = new SimpleTree();
+//        Pattern pattern = new KochIsle();
 
         startingX = pattern.getStartingX();
         startingY = pattern.getStartingY();
         length = pattern.getLength();
         lengthDivisor = pattern.getLengthDivisor();
-        axiom = pattern.getAxiom();
         rules = pattern.getRules();
         rotAngle = pattern.getRotAngle();
 
-        generationCount = 3;
+        generationCount = 4;
         rotationManager = new RotationManager(pattern.getAngle());
 
-        lSystem = axiom;
+        lSystem = pattern.getAxiom();
     }
-    //todo check maybe shouldnt save angle
+
     private void drawLSystem() {
         for (int i = 0; i < lSystem.length(); i++) {
             switch (lSystem.charAt(i)) {
-                case 'F' -> stepAndDrawLine();
+                case 'F', 'G' -> stepAndDrawLine();
                 case 'f' -> step();
                 case '+' -> rotateRight(rotAngle);
                 case '-' -> rotateLeft(rotAngle);
@@ -158,14 +157,15 @@ public class Renderer extends AbstractRenderer {
         startingY = y2;
     }
 
-    //+
-    private void rotateRight(int angle) {
-        rotationManager.setAngle((rotationManager.getAngle() + angle + 180));
-    }
 
     //-
     private void rotateLeft(int angle) {
         rotationManager.setAngle((rotationManager.getAngle() + angle));
+    }
+
+    //+
+    private void rotateRight(int angle) {
+        rotationManager.setAngle((rotationManager.getAngle() - angle));
     }
 
     private void saveState() {
--- src/project/patterns/SimpleTree.java
+++ src/project/patterns/SimpleTree.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 public class SimpleTree extends Pattern {
     public SimpleTree() {
-        super(0f, -0.8f, 90, 300, 1.2f, 3, "F",
-                List.of(new Rule('F', "F[-F]F[+F][F]")));
+        super(0f, -0.8f, 90, 45, 1.5f, 2, "F",
+                List.of(
+                        new Rule('F', "G[+F]-F"),
+                        new Rule('G', "GG")
+                ));
     }
 }
 
